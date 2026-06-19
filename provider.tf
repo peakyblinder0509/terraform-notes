a69af265497ed5d38eae09fd0b468a9501039822
peakyblinder0509 <connect2ikram509@gmail.com>
2026-06-19 18:32:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
-provider "aws" {
-  region = "eu-north-1"   # unga region
-}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 6.42.0"
     }
   }
+}
+
+provider "aws" {
+  region = "ap-south-1"
 }
